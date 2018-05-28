b8f6a999285b3a1bcefbae789678d06ae868ae9e
Brian Sayler <sayler.b@gmail.com>
2018-05-28 14:28:52 -0500

    update run script to ask for sudo password

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 name=$1
 
 if [[ -n "$name" ]]; then
-    ansible-playbook main.yml -i hosts.ini --vault-password-file ./.vault_pass.txt --tags $name -vvv
+    ansible-playbook main.yml \
+      -i hosts.ini \
+      --vault-password-file ./.vault_pass.txt \
+      --tags $name \
+      -vvv \
+      --ask-become-pass
 else
    echo "You didn't specify any tags!"
 fi
