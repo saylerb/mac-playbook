ac3a583eec9476a887038f7e2c4312991f1b8037
Brian Sayler <saylerb@users.noreply.github.com>
2022-04-23 17:15:49 -0600

    more readable run script

--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
-name=$1
+tags=$1
 
 # Activate python virtual environment
 source .venv/bin/activate
 
-if [[ -n "$name" ]]; then
-    ansible-playbook main.yml \
-      -i hosts.ini \
-      --vault-password-file ./.vault_pass.txt \
-      --tags $name \
-      -vvv \
-      --ask-become-pass \
-      --step
+if [[ -n "$tags" ]]; then
+  ansible-playbook main.yml \
+    -i hosts.ini \
+    --vault-password-file ./.vault_pass.txt \
+    --tags $tags \
+    -vvv \
+    --ask-become-pass \
+    --step
 else
-   echo "You didn't specify any tags! Running all tasks"
-   ansible-playbook main.yml \
-      -i hosts.ini \
-      --vault-password-file ./.vault_pass.txt \
-      -vvv \
-      --ask-become-pass \
-      --step
+  echo "You didn't specify any tags! Running all tasks"
+  ansible-playbook main.yml \
+    -i hosts.ini \
+    --vault-password-file ./.vault_pass.txt \
+    -vvv \
+    --ask-become-pass \
+    --step
 fi
